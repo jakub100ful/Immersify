6728c5b845520288934b4067136ba69e60df5ebd
Jakub <jakub.bebenek@gmail.com>
2021-05-12 14:26:52 +0100

    Classification Algorithm Implemented

--- app/classify.py
+++ app/classify.py
@@ -1,16 +1,26 @@
 import webvtt
 import text2emotion as te
+import operator
 
 
 def decideEmotion(text):
     emotion_list = te.get_emotion(text)
-    print(emotion_list)
+    selected_emotion = max(emotion_list.items(),
+                           key=operator.itemgetter(1))[0]
+    if(selected_emotion):
+        return selected_emotion
 
 
 def classifySubtitle(video_id):
     subtitle_path = "static/videos/{0}/{1}_subtitles.vtt".format(
         video_id, video_id)
 
-    for caption in webvtt.read(subtitle_path):
+    vtt = webvtt.read(subtitle_path)
+
+    for caption in vtt:
         print(caption.text)
-        decideEmotion(caption.text)
+        emotion = decideEmotion(caption.text)
+        caption.text = '<c.{0}>{1}</c>'.format(emotion.lower(), caption.text)
+
+    vtt.save(
+        './static/videos/{}/{}_emotive_subtitles.vtt'.format(video_id, video_id))
