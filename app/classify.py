56374d3992e67dd0c1879d6983858fbbff92ef7f
Jakub <jakub.bebenek@gmail.com>
2021-05-14 16:58:51 +0100

    Modifying Emotion Styles

--- app/classify.py
+++ app/classify.py
@@ -4,7 +4,9 @@ import operator
 
 
 def decideEmotion(text):
+    # TODO: Filter
     emotion_list = te.get_emotion(text)
+    print(emotion_list)
     selected_emotion = max(emotion_list.items(),
                            key=operator.itemgetter(1))[0]
     if(selected_emotion):
@@ -20,8 +22,8 @@ def classifySubtitle(video_id):
     for caption in vtt:
         print(caption.text)
         emotion = decideEmotion(caption.text)
-        caption.text = '<{0}>{1}</{2}>'.format(
-            emotion.lower(), caption.text, emotion.lower())
+        caption.text = "<p id='{0}'>{1}</p>".format(
+            emotion.lower(), caption.text)
 
     vtt.save(
         './static/videos/{}/{}_emotive_subtitles.vtt'.format(video_id, video_id))
